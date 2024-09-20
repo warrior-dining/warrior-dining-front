5b6b00fd9154d17a895e31b8dd1a305b2ead7784
myin98 <dnjsdydpf@gmail.com>
2024-09-20 18:23:59 +0900

    [#1] 문의하기 - 자주 묻는 질문 ui 구현

---
+++ src/containers/InquiryFaq.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import '../css/default.css';
+import '../css/inquiry.css';
+
+
+const InquiryFaq = () => {
+    return (
+        <>
+        <main class="container">
+    <div class="main-content">
+
+    <section>
+      <h2 class="section-title">자주 묻는 질문</h2>
+      <div class="faq-container">
+        <div class="faq-item">
+          <h3>Q1: 서비스 이용 방법은?</h3>
+          <p>서비스 이용 방법에 대한 상세한 설명이 여기에 들어갑니다. 사용자는 여기를 통해 어떻게 서비스를 이용하는지 알 수 있습니다.</p>
+        </div>
+        <div class="faq-item">
+          <h3>Q2: 결제는 어떻게 하나요?</h3>
+          <p>결제 방법에 대한 상세한 설명이 여기에 들어갑니다. 사용자는 여기를 통해 결제 방법을 알 수 있습니다.</p>
+        </div>
+        <div class="faq-item">
+          <h3>Q3: 예약 변경/취소는 어떻게 하나요?</h3>
+          <p>예약 변경 및 취소 방법에 대한 상세한 설명이 여기에 들어갑니다. 사용자는 여기를 통해 예약 변경 및 취소 방법을 알 수 있습니다.</p>
+        </div>
+        <div class="faq-item">
+          <h3>Q4: 회원 가입은 어떻게 하나요?</h3>
+          <p>회원 가입 방법에 대한 상세한 설명이 여기에 들어갑니다. 사용자는 여기를 통해 회원 가입 방법을 알 수 있습니다.</p>
+        </div>
+        <div class="faq-item">
+          <h3>Q5: 문의는 어떻게 하나요?</h3>
+          <p>문의 방법에 대한 상세한 설명이 여기에 들어갑니다. 사용자는 여기를 통해 문의 방법을 알 수 있습니다.</p>
+        </div>
+        <div class="faq-item">
+          <h3>Q4: 회원 가입은 어떻게 하나요?</h3>
+          <p>회원 가입 방법에 대한 상세한 설명이 여기에 들어갑니다. 사용자는 여기를 통해 회원 가입 방법을 알 수 있습니다.</p>
+        </div>
+        <div class="faq-item">
+          <h3>Q5: 문의는 어떻게 하나요?</h3>
+          <p>문의 방법에 대한 상세한 설명이 여기에 들어갑니다. 사용자는 여기를 통해 문의 방법을 알 수 있습니다.</p>
+        </div>
+      </div>
+    </section>
+
+    <div class="header-buttons">
+      <a href="/" class="header-button">문의하기</a>
+      <a href="/" class="header-button">내 문의내역</a>
+    </div>
+  </div>
+</main>
+  
+        </>
+
+    );
+};
+
+
+export default InquiryFaq;
