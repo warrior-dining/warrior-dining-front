0530b33d1c2c81aeb35cd65ed69d1cc77784d560
myin98 <dnjsdydpf@gmail.com>
2024-09-23 17:54:08 +0900

    [#1] 음식점 찾기, 문의 하기 링크 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Footer from './components/Footer';
 import Main from './containers/Main';
 import TopReservation from './containers/TopReservation';
 import MonthBest from './containers/MonthBest';
+import InquiryFaq from './containers/InquiryFaq';
+import RestaurantList from './containers/RestaurantList';
 import './css/home.css';
 import './css/default.css';
 
@@ -19,6 +21,8 @@ function App() {
       <Routes>
         <Route path="/TopReservation" element={<TopReservation />} /> 
         <Route path="/MonthBest" element={<MonthBest />} />
+        <Route path="/RestaurantList" element={<RestaurantList />} />
+        <Route path="/InquiryFaq" element={<InquiryFaq />} />
         {/* 다른 경로들도 여기 추가할 수 있습니다. */}
         <Route path="/" element={<Main />} />
       </Routes>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,10 +6,10 @@ function NavBar() {
     <div className="nav-bar">
       <div className="container">
         <ul>
-        <li><Link to="/TopReservation">예약 TOP</Link></li> {/* Link로 수정 */}
+        <li><Link to="/TopReservation">예약 TOP</Link></li>
           <li><Link to="/MonthBest">이달의 맛집</Link></li>
-          <li><a href="/reservation">예약</a></li>
-          <li><a href="#">문의</a></li>
+          <li><Link to="/RestaurantList">음식점 찾기</Link></li>
+          <li><Link to="/InquiryFaq">문의</Link></li>
           <li className="search-bar">
             <input type="text" placeholder="지역 또는 레스토랑 검색" />
             <button>검색</button>
