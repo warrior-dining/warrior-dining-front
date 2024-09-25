f70b6feb83dfcaaf63b67f565af96a8cb392d393
myin98 <dnjsdydpf@gmail.com>
2024-09-25 18:16:44 +0900

    [#1] 관리자 페이지 이동시 네브바 변경 이벤트 추가

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import NavBar from './components/NavBar';
@@ -8,22 +8,39 @@ import TopReservation from './containers/TopReservation';
 import MonthBest from './containers/MonthBest';
 import InquiryFaq from './containers/InquiryFaq';
 import RestaurantList from './containers/RestaurantList';
+import InquiryCreate from './containers/InquiryCreate';
+import Admin from './adminContainers/Admin';
+import NavBarAdmin from './adminContainers/NavBarAdmin';
 import './css/home.css';
 import './css/default.css';
 
 
 function App() {
+   const [isAdmin, setIsAdmin] = useState(false);
+ 
+  useEffect(()=> {
+
+  }, []);
+
   return (
     <Router>
     <div className="App">
-      <Header />
-      <NavBar />
-      <Routes>
-        <Route path="/TopReservation" element={<TopReservation />} /> 
-        <Route path="/MonthBest" element={<MonthBest />} />
-        <Route path="/RestaurantList" element={<RestaurantList />} />
-        <Route path="/InquiryFaq" element={<InquiryFaq />} />
-        {/* 다른 경로들도 여기 추가할 수 있습니다. */}
+      <Header adminClick={setIsAdmin} />
+      {isAdmin ? (
+        <NavBarAdmin />
+      ) : (
+        <>      
+        <NavBar />      
+      </>
+    )}
+        <Routes>
+          <Route path="/topreservation" element={<TopReservation />} /> 
+          <Route path="/monthbest" element={<MonthBest />} />
+          <Route path="/restaurantlist" element={<RestaurantList />} />
+          <Route path="/inquiryfaq" element={<InquiryFaq />} />
+          <Route path="/inquirycreate" element={<InquiryCreate />} />
+          <Route path="/admin" element={<Admin />} />
+          {/* 다른 경로들도 여기 추가할 수 있습니다. */}
         <Route path="/" element={<Main />} />
       </Routes>
       <Footer />
