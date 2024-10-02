aea80dc2097ec24ded4fa267d0f74646e49fa43c
mirinae <enkim0401@naver.com>
2024-10-02 16:53:12 +0900

    [#8] 관리자 문의사항 리스트,디테일 구현

--- src/adminContainers/InquiriesDetailAdmin.js
+++ src/adminContainers/InquiriesDetailAdmin.js
@@ -1,31 +1,51 @@
 import React from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import '../css/InquiriesDetailAdmin.css';
 
+const host = "http://localhost:8080/admin/inquiries/";
+const InquirieDtailsAdmin = ({ inquiryId }) => {
+    const {id} = useParams();
+    const inquiriesId = Number(id);
+    const [data, setData] = useState(null);
+    useEffect(() => {
+        axios.get(host + inquiriesId)
+        .then(res => {
+            console.log(res);
+            setData(res.data.results);
 
-const InquirieDtailsAdmin = () => {
+        })
+        .catch(error => console.log(error));
+    }, []);
+
+    if (!data) {
+        return <div>Loading...</div>; // 데이터가 로드되기 전까지 로딩 메시지 표시
+    }
+
     return (
         <>
             <main>
-                <div class="container">
+                <div className="container">
 
                     <h2 className="main-title">문의 사항 상세</h2>
 
-                    <div class="details-container">
-                        <h3>문의 제목</h3>
-                        <div class="info">
-                            <p><label>작성자:</label> <span class="content">홍길동</span></p>
-                            <p><label>날짜:</label> <span class="content">2024-08-21</span></p>
+                    <div className="details-container">
+                        <h3>{data.title}</h3>
+                        <div className="info">
+                            <p><label>작성자:</label> <span className="content">{data.user.name}</span></p>
+                            <p><label>날짜:</label> <span className="content">{data.createdAt}</span></p>
                         </div>
-                        <div class="content">
+                        <div className="info">
                             <p><label>문의 내용:</label></p>
-                            <p>여기에 문의 내용이 들어갑니다. 예를 들어, 고객이 음식점의 서비스에 대해 질문한 내용이 포함될 수 있습니다.</p>
+                            <p>{data.content}</p>
                         </div>
-                        <div class="reply-container">
+                        <div className="reply-container">
                             <h4>답변 작성</h4>
                             <form>
                                 <textarea placeholder="여기에 답변을 작성하세요..."></textarea>
                                 <button type="submit">답변 저장</button>
-                            </form>
+                            </form> 
                         </div>
                     </div>
                 </div>
