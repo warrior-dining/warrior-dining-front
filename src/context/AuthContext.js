01a1f65ff628f3bc74a82435de27d6cac1463101
o-tao <dhtkdals369@gmail.com>
2024-10-08 15:55:37 +0900

    [#19] Context로 토큰 전역설정

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,20 +1,33 @@
 import React, { createContext, useContext, useState } from 'react';
+import { jwtDecode } from 'jwt-decode';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [token, setToken] = useState(null);
+    const [auth, setAuth] = useState([]);
+    const [isAdminNav, setIsAdminNav] = useState(false);
 
     const saveToken = (token) => {
         setToken(token);
+        try {
+            const decodedToken = jwtDecode(token);
+            console.log('디코딩된 토큰:', decodedToken);
+            setAuth(decodedToken.auth);
+            setIsAdminNav(decodedToken.auth.includes('ADMIN'));
+            console.log('저장된 권한:', decodedToken.auth);
+        } catch (error) {
+            console.error('토큰 디코딩 오류:', error);
+        }
     };
 
     const clearToken = () => {
         setToken(null);
+        setAuth([]);
     };
 
     return (
-        <AuthContext.Provider value={{ token, saveToken, clearToken }}>
+        <AuthContext.Provider value={{ token, permissions: auth, saveToken, clearToken }}>
             {children}
         </AuthContext.Provider>
     );
