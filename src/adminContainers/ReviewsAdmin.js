167b586466c776c2baf653cd56389abd0722f434
JJAJJAKIM <csm9121@gmail.com>
2024-10-13 01:17:50 +0900

    [#35] [관리자] - 리뷰 관리 - 리뷰 전체 목록 페이지 검색기능 구현 완료.

--- src/adminContainers/ReviewsAdmin.js
+++ src/adminContainers/ReviewsAdmin.js
@@ -1,161 +1,213 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, {useRef, useState} from 'react';
+import {useEffect} from 'react';
 import axios from 'axios';
 import '../css/reviewsAdmin.css';
 
 const host = "http://localhost:8080/api/admin/reviews/";
 
-const ReviewList = () => {
-  const [expandedRowIds, setExpandedRowIds] = useState([]);
-  const [data, setData] = useState([]);
+const ReviewList = ({data, setData}) => {
+    const [expandedRowIds, setExpandedRowIds] = useState([]);
 
-  useEffect(() => {
-    axios.get(host)
-      .then(res => {
-        console.log(res);
-        setData(res.data.data);
-      })
-      .catch(error => console.log(error));
-  }, []);
+    const handleRowClick = (id) => {
+        setExpandedRowIds((prev) =>
+            prev.includes(id) ? prev.filter(rowId => rowId !== id) : [...prev, id]
+        );
+    };
 
-  if (!data || (Array.isArray(data) && data.length === 0)) {
-    return <div>Loading...</div>; // 데이터가 로드되기 전까지 로딩 메시지 표시
-  }
+    const getStars = (rating) => {
+        const starCount = parseInt(rating, 10); // 평점을 정수로 변환
+        let stars = '';
+        for (let i = 0; i < 5; i++) {
+            stars += i < starCount ? '★' : '☆'; // 평점에 따라 채워진 별과 빈 별을 생성
+        }
+        return stars;
+    };
 
-  const handleRowClick = (id) => {
-    setExpandedRowIds((prev) => 
-      prev.includes(id) ? prev.filter(rowId => rowId !== id) : [...prev, id]
-    );
-  };
+    const truncateContent = (content) => {
+        if (content.length > 5) {
+            return content.slice(0, 5) + '...'; // 5글자 이상이면 잘라내고 ... 추가
+        }
+        return content;
+    };
 
-  const getStars = (rating) => {
-    const starCount = parseInt(rating, 10); // 평점을 정수로 변환
-    let stars = '';
-  
-    for (let i = 0; i < 5; i++) {
-      stars += i < starCount ? '★' : '☆'; // 평점에 따라 채워진 별과 빈 별을 생성
-    }
-  
-    return stars;
-  };
+    const handleUpdateStatus = (id) => {
+        setData(prevData => prevData.map(item =>
+            item.id === id ? {...item, isDeleted: true} : item
+        ));
 
-  const truncateContent = (content) => {
-    if (content.length > 5) {
-      return content.slice(0, 5) + '...'; // 5글자 이상이면 잘라내고 ... 추가
-    }
-    return content;
-  };
+        axios.patch(`${host}/${id}`, {status: 1}) // 상태 업데이트를 위한 요청
+            .then(res => {
+                window.location.reload();
+            })
+            .catch(error => {
+                console.log('Axios error:', error);
+                if (error.response) {
+                    console.log('Response data:', error.response.data);
+                    console.log('Response status:', error.response.status);
+                    console.log('Response headers:', error.response.headers);
+                }
+            });
+    };
 
-  const handleUpdateStatus = (id) => {
-    setData(prevData => prevData.map(item => 
-      item.id === id ? { ...item, isDeleted: true } : item
-    ));
-
-    axios.patch(`${host}/${id}`, { status: 1 }) // 상태 업데이트를 위한 요청
-        .then(res => {
-            window.location.reload();
-        })
-        .catch(error => {
-          console.log('Axios error:', error);
-          if (error.response) {
-              console.log('Response data:', error.response.data);
-              console.log('Response status:', error.response.status);
-              console.log('Response headers:', error.response.headers);
-          }
-      });
-};
-
-
-  return (
-    <>
-      <table className="review-table">
-        <thead>
-          <tr>
-            <th>리뷰 ID</th>
-            <th>사용자 이름</th>
-            <th>음식점명</th>
-            <th>평점</th>
-            <th>리뷰 내용</th>
-            <th>리뷰 날짜</th>
-            <th>작업</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.map((row, index) => (
-            <React.Fragment key={index}>
-              <tr onClick={() => handleRowClick(row.id)}>
-                <td>{row.id}</td>
-                <td>{row.user.name}</td>
-                <td>{row.place.name}</td>
-                <td>{getStars(row.rating)}</td>
-                <td>{truncateContent(row.content)}</td>
-                <td>{row.createdAt}</td>
-                <td className="actions">
-                  {row.status === '0' ? '1' : (
-                    row.deleted ? 
-                    '처리됨' :
-                      <button className="delete"  onClick={() => handleUpdateStatus(row.id, { status: 1 })}>삭제</button>
-                      
-                    
-                  )}
-                </td>
-              </tr>
-              {expandedRowIds.includes(row.id) && (
-                <tr className="review-detail">
-                  <td colSpan="7">
-                    <div className="detail-header">상세 내용:</div>
-                    <p>{row.content}</p>
-                  </td>
+    return (
+        <>
+            <table className="review-table">
+                <thead>
+                <tr>
+                    <th>리뷰 ID</th>
+                    <th>사용자 이름</th>
+                    <th>음식점명</th>
+                    <th>평점</th>
+                    <th>리뷰 내용</th>
+                    <th>리뷰 날짜</th>
+                    <th>작업</th>
                 </tr>
-              )}
-            </React.Fragment>
-          ))}
-        </tbody>
-      </table>
-    </>
-  );
+                </thead>
+                <tbody>
+                {data.map((row, index) => (
+                    <React.Fragment key={index}>
+                        <tr onClick={() => handleRowClick(row.id)}>
+                            <td>{row.id}</td>
+                            <td>{row.user.name}</td>
+                            <td>{row.place.name}</td>
+                            <td>{getStars(row.rating)}</td>
+                            <td>{truncateContent(row.content)}</td>
+                            <td>{row.createdAt}</td>
+                            <td className="actions">
+                                {row.status === '0' ? '1' : (
+                                    row.deleted ?
+                                        '처리됨' :
+                                        <button className="delete"
+                                                onClick={() => handleUpdateStatus(row.id, {status: 1})}>삭제</button>
+                                )}
+                            </td>
+                        </tr>
+                        {expandedRowIds.includes(row.id) && (
+                            <tr className="review-detail">
+                                <td colSpan="7">
+                                    <div className="detail-header">상세 내용:</div>
+                                    <p>{row.content}</p>
+                                </td>
+                            </tr>
+                        )}
+                    </React.Fragment>
+                ))}
+                </tbody>
+            </table>
+        </>
+    );
 };
 
 const ReviewsAdmin = () => {
-  return (
-    <>
-      <main>
-        <div className="container">
-          <h2 className="main-title">사용자 리뷰 전체 목록</h2>
+    const [data, setData] = useState([]);
+    const [searchKeyword, setSearchKeyword] = useState('');
+    const [searchType, setSearchType] = useState('user');
+    const searchKeywordRef = useRef();
+    const [page, setPage] = useState(0);
+    const [pageSize, setPageSize] = useState(10);
+    const [totalPages, setTotalPages] = useState(0);
+    const [sortType, setSortType] = useState('');
+    
+    useEffect(() => {
+        let url = `${host}?searchtype=${searchType}&searchkeyword=${searchKeyword}&page=${page}&size=${pageSize}&sorttype=${sortType}`;
+        axios.get(url)
+            .then(res => {
+                console.log(res);
+                setData(res.data.status ? res.data.results.content : []);
+                setTotalPages(res.data.status ? res.data.results.totalPages : 0);
+            })
+            .catch(error => console.log(error));
+    }, [ page, pageSize, searchKeyword, sortType ]);
 
-          <div className="filter-bar">
-            <select>
-              <option>날짜</option>
-              <option>2024-08-25</option>
-              <option>2024-08-24</option>
-            </select>
-            <select>
-              <option>평점</option>
-              <option>★★★</option>
-              <option>★★★★</option>
-            </select>
-            <select>
-              <option>상태</option>
-              <option>승인 대기</option>
-              <option>승인됨</option>
-              <option>삭제됨</option>
-            </select>
-            <select>
-              <option>정렬 기준</option>
-              <option>최신순</option>
-              <option>오래된 순</option>
-              <option>평점 높은 순</option>
-            </select>
-          </div>
-          <div className="search-bar">
-            <input type="text" placeholder="리뷰 검색" />
-            <button>검색</button>
-          </div>
-          <ReviewList />
-        </div>
-      </main>
-    </>
-  );
+    const searchEvent = (e) => {
+        e.preventDefault();
+        if(searchKeyword === null) {
+            alert("검색어를 입력하세요.");
+            return;
+        }
+        setPage(0); // 검색할 때 페이지를 0으로 초기화
+        setSearchKeyword(searchKeywordRef.current.value);
+    };
+
+    const getPaginationNumbers = () => {
+        const maxPagesToShow = 5; // 화면에 보여줄 페이지 갯수
+        const startPage = Math.max(0, page - Math.floor(maxPagesToShow / 2));
+        const endPage = Math.min(totalPages, startPage + maxPagesToShow);
+        return Array.from({ length: endPage - startPage }, (_, index) => startPage + index);
+    };
+    const paginationNumbers = getPaginationNumbers();
+
+    if (!data || (Array.isArray(data) && data.length === 0)) {
+        return <div>Loading...</div>; // 데이터가 로드되기 전까지 로딩 메시지 표시
+    }
+    return (
+        <>
+            <main>
+                <div className="container">
+                    <h2 className="main-title">사용자 리뷰 전체 목록</h2>
+                    <div className="admin-filter-search-container">
+                        <div className="admin-filter-bar">
+                            <select value={sortType} onChange={(e) => { setSortType(e.target.value) }}>
+                                <option>평점</option>
+                                <option value="1">★</option>
+                                <option value="2">★★</option>
+                                <option value="3">★★★</option>
+                                <option value="4">★★★★</option>
+                                <option value="5">★★★★★</option>
+                            </select>
+                            <select value={sortType} onChange={(e) => { setSortType(e.target.value) }}>
+                                <option>상태</option>
+                                <option value="true">활성화</option>
+                                <option value="false">비활성화</option>
+                            </select>
+                            <select value={sortType} onChange={(e) => { setSortType(e.target.value) }}>
+                                <option>정렬 기준</option>
+                                <option value="desc">최신순</option>
+                                <option value="asc">오래된 순</option>
+                            </select>
+                        </div>
+                        <form onSubmit={searchEvent}>
+                            <div className="admin-search-bar">
+                                <select value={searchType} onChange={(e) => { setSearchType(e.target.value) }}>
+                                    <option value="user">사용자 이름</option>
+                                    <option value="place">음식점명</option>
+                                    <option value="content">리뷰 내용</option>
+                                </select>
+                                <input type="text" placeholder="리뷰 검색" ref={searchKeywordRef}/>
+                                <button type="submit">검색</button>
+                            </div>
+                        </form>
+                    </div>
+                    <ReviewList data={data} setData={setData}/>
+                    <div className="pagination">
+                        <a href="#"
+                           onClick={(e) => {
+                               e.preventDefault();
+                               if (page > 0) {
+                                   setPage(page - 1);
+                               }
+                           }}
+                           disabled={page === 0}> 이전 </a>
+                        {paginationNumbers.map((num) => (
+                            <a key={num} href="#" className={num === page ? "active" : ""}
+                               onClick={(e) => {
+                                   e.preventDefault();
+                                   setPage(num);
+                               }}> {num + 1} </a>
+                        ))}
+                        <a href="#"
+                           onClick={(e) => {
+                               e.preventDefault();
+                               if (page < totalPages - 1) {
+                                   setPage(page + 1);
+                               }
+                           }}
+                           disabled={page >= totalPages - 1}> 다음 </a>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 };
 
 export default ReviewsAdmin;
