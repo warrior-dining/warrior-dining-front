09b37188b6ca1f7a3a65478e9811aa52488cf3b1
mirinae <enkim0401@naver.com>
2024-10-14 17:45:06 +0900

    [#43]관리자 예약페이지 상태값으로 정렬 및 검색기능, css수정

--- src/adminContainers/ReservationAdmin.js
+++ src/adminContainers/ReservationAdmin.js
@@ -1,48 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import '../css/reservationManagement.css';
 
 const host = "http://localhost:8080/api/admin/reservations/";
 
 const ReservationAdmin = () => {
-    const [currentPage, setCurrentPage] = useState(1);
-    const [currentStatusFilter, setCurrentStatusFilter] = useState('전체');
-    const [searchQuery, setSearchQuery] = useState('');
-    const [filteredReservations, setFilteredReservations] = useState([]);
+    const [sortType, setSortType] = useState('');
     const [expandedReservationId, setExpandedReservationId] = useState(null);
     const [data, setData] = useState([]);
     const [totalPages, setTotalPages] = useState(0);
-    const [status, setStatus] = useState([]);
+    const [page, setPage] = useState(0);
+    const [pageSize, setPageSize] = useState(5);
+    const searchKeywordRef= useRef('');
+    const [searchType, setSearchType] = useState('id');
+    const [searchKeyword, setSearchKeyword] = useState('');
 
-    let url = host + `?page=${currentPage - 1}&size=5&status=${status}`; // currentPage - 1
 
     useEffect(() => {
+         // URL 정의 type=${searchType}&keyword=${searchKeyword}&
+        let url = `${host}?page=${page}&size=${pageSize}&status=${sortType}&type=${searchType}&keyword=${searchKeyword}`;
         axios.get(url)
             .then(res => {
-                setData(res.data.data.content);
-                setTotalPages(res.data.data.totalPages);
+                console.log(res);
+                setData(res.data.results.content);
+                setTotalPages(res.data.results.totalPages);
             })
             .catch(error => console.log(error));
-    }, [currentPage]);
-
-    useEffect(() => {
-        renderReservations();
-    }, [currentStatusFilter, data]); // searchQuery는 제외
-
-    const renderReservations = () => {
-        const filtered = data.filter(row => {
-            // 상태 필터링
-            if (currentStatusFilter !== '전체' && row.code.value !== currentStatusFilter) {
-                return false;
-            }
-            // 검색 필터링
-            if (searchQuery && !row.user.name.includes(searchQuery)) {
-                return false;
-            }
-            return true;
-        });
-        setFilteredReservations(filtered);
-    };
+    }, [page, pageSize, sortType, searchKeyword]);
 
     const handleUpdateStatus = (id) => {
         const confirmCancel = window.confirm("예약을 취소하시겠습니까?");
@@ -51,43 +35,42 @@ const ReservationAdmin = () => {
         }
         axios.patch(`${host}${id}`, { status: 14 })
             .then(res => {
-                setData(prevData => prevData.map(item => 
-                    item.id === id ? { ...item, code: { value: '취소' } } : item
-                ));
-                renderReservations();
+                alert("예약이 정상적으로 취소되었습니다.")
+                setSortType('13');
             })
             .catch(error => {
                 console.log('Axios error:', error);
             });
     };
 
-    const filterReservations = (status) => {
-        setCurrentStatusFilter(status);
-        setCurrentPage(1); // 페이지를 1로 리셋
-    };
-
-    const handleSearch = (event) => {
-        setSearchQuery(event.target.value);
-        setCurrentPage(1);
+    const searchEvent = (e) => {
+        console.log("호출되나?")
+        e.preventDefault();
+        if(searchKeyword === null) {
+            alert("검색어를 입력하세요.");
+            return;
+        }
+        setPage(0);
+        setSearchKeyword(searchKeywordRef.current.value);
     };
 
     const toggleDetails = (id) => {
         setExpandedReservationId(expandedReservationId === id ? null : id);
     };
 
-    if (!data || data.length === 0) {
-        return <div>Loading...</div>;
-    }
-
     const getPaginationNumbers = () => {
         const maxPagesToShow = 5;
-        const startPage = Math.max(0, currentPage - Math.floor(maxPagesToShow));
+        const startPage = Math.max(0, page - Math.floor(maxPagesToShow));
         const endPage = Math.min(totalPages, startPage + maxPagesToShow);
 
         return Array.from({ length: endPage - startPage }, (_, index) => startPage + index);
     };
 
     const paginationNumbers = getPaginationNumbers();
+    
+    if (!data || data.length === 0) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <main>
@@ -96,19 +79,26 @@ const ReservationAdmin = () => {
                     <h2 className="main-title">예약 목록</h2>
                     <div className="filter-search-container">
                         <div className="filter-buttons">
-                            <button onClick={() => filterReservations('전체')}>전체</button>
-                            <button onClick={() => filterReservations('확정')}>확정</button>
-                            <button onClick={() => filterReservations('대기')}>대기</button>
-                            <button onClick={() => filterReservations('취소')}>취소</button>
-                            <button onClick={() => filterReservations('완료')}>완료</button>
+                            <button onClick={() => setSortType('')}>전체</button>
+                            <button onClick={() => setSortType('15')}>확정</button>
+                            <button onClick={() => setSortType('14')}>대기</button>
+                            <button onClick={() => setSortType('13')}>취소</button>
+                            <button onClick={() => setSortType('12')}>완료</button>
                         </div>
-                        <div className="reservation-search-bar">
-                            <input type="text" id="search-input" placeholder="검색어를 입력하세요" onChange={handleSearch} />
-                            <button onClick={renderReservations}>검색</button>
-                        </div>
+                        <form  onSubmit={searchEvent}>
+                            <div className="reservation-search-bar">
+                                <select value={searchType} onChange={(e)=> {setSearchType(e.target.value)}}>
+                                    <option value="id">예약번호</option>
+                                    <option value="name">예약자 이름</option>
+                                    <option value="place">식당</option>
+                                </select>
+                                <input type="text" id="search-input" placeholder="검색어를 입력하세요"  ref={searchKeywordRef}/>
+                                <button type="submit">검색</button>
+                            </div>
+                        </form>
                     </div>
                     <div id="reservation-container">
-                        {filteredReservations.map((row) => (
+                        {data.map((row) => (
                             <div className="reservation-item" key={row.id} onClick={() => toggleDetails(row.id)} style={{ cursor: 'pointer' }}>
                                 <h3>예약 ID: {row.id}</h3>
                                 <p>
@@ -135,13 +125,25 @@ const ReservationAdmin = () => {
                         ))}
                     </div>
                     <div className="reservation-pagination">
-                        <a href="#" onClick={(e) => { e.preventDefault(); if (currentPage > 1) setCurrentPage(currentPage - 1); }}>이전</a>
+                        <a href="#"
+                            onClick={(e) => {
+                                e.preventDefault();
+                               if(page > 0){setPage(page - 1);}
+                            }}
+                            disabled={page === 0}> 이전 </a>
                         {paginationNumbers.map((num) => (
-                            <a key={num} href="#" className={num + 1 === currentPage ? "active" : ""} onClick={(e) => { e.preventDefault(); setCurrentPage(num + 1); }}>
-                                {num + 1}
-                            </a>
+                            <a key={num} href="#" className={num === page ? "active" : ""}
+                               onClick={(e) => {
+                                   e.preventDefault();
+                                   setPage(num);
+                               }}> {num + 1} </a>
                         ))}
-                        <a href="#" onClick={(e) => { e.preventDefault(); if (currentPage < totalPages) setCurrentPage(currentPage + 1); }}>다음</a>
+                        <a href="#"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                if(page < totalPages-1){ setPage(page + 1);}
+                            }}
+                            disabled={page >= totalPages - 1}> 다음 </a>
                     </div>
                 </section>
             </div>
