82bf3d3002a6e60adb04464ef6ce299920f0bbb0
o-tao <dhtkdals369@gmail.com>
2024-10-17 17:04:36 +0900

    [#54] 불필요 명령어 제거

--- src/components/MypageSidebar.js
+++ src/components/MypageSidebar.js
@@ -1,53 +1,48 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import '../css/mypageMutual.css';
 
 const MypageSidebar = () => {
-    return ( 
+    return (
         <div className="mypage-sidebar">
             <h2>마이페이지</h2>
             <ul>
                 <li>
-                    <NavLink 
-                        to="/mypage/main" 
-                        className={({ isActive }) => (isActive ? 'active' : '')}
-                        exact
+                    <NavLink
+                        to="/mypage/main"
+                        className={({isActive}) => (isActive ? 'active' : '')}
                     >
                         내정보
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink 
-                        to="/mypage/reservationlist" 
-                        className={({ isActive }) => (isActive ? 'active' : '')}
-                        exact
+                    <NavLink
+                        to="/mypage/reservationlist"
+                        className={({isActive}) => (isActive ? 'active' : '')}
                     >
                         예약 내역
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink 
-                        to="/mypage/reviewlist" 
-                        className={({ isActive }) => (isActive ? 'active' : '')}
-                        exact
+                    <NavLink
+                        to="/mypage/reviewlist"
+                        className={({isActive}) => (isActive ? 'active' : '')}
                     >
                         리뷰 관리
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink 
-                        to="/mypage/bookmark" 
-                        className={({ isActive }) => (isActive ? 'active' : '')}
-                        exact
+                    <NavLink
+                        to="/mypage/bookmark"
+                        className={({isActive}) => (isActive ? 'active' : '')}
                     >
                         즐겨찾기
                     </NavLink>
                 </li>
                 <li>
-                    <NavLink 
-                        to="/mypage/inquiry" 
-                        className={({ isActive }) => (isActive ? 'active' : '')}
-                        exact
+                    <NavLink
+                        to="/mypage/inquiry"
+                        className={({isActive}) => (isActive ? 'active' : '')}
                     >
                         내 문의내역
                     </NavLink>
