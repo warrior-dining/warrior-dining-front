39a0b2b571358faa5bbdc86e817b827e3310708f
mirinae <enkim0401@naver.com>
2024-10-17 15:19:53 +0900

    ﻿[#51] 마이페이지 문의사항 리스트 불러오기 및 수정

--- src/containers/InquiryDetail.js
+++ src/containers/InquiryDetail.js
@@ -1,24 +1,72 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../css/default.css';
 import '../css/inquiry.css';
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
 
+const host = "http://localhost:8080/api/member/inquiries/info/"
 const InquiryDetail = () => {
+    const {id} = useParams();
+    const inquiryId = Number(id);
+    const [data, setData] = useState([]);
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+    const navigate = useNavigate();
+
+    useEffect(()=> {
+        const url = host + inquiryId;
+        axios.get(url)
+        .then(res => {
+            setData(res.data.results);
+            setTitle(res.data.results.title);
+            setContent(res.data.results.content);
+        })
+        .catch(error => console.log(error));
+    }, [inquiryId]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const updateData = {"title": title, "content": content}
+        axios.put(`${host}${inquiryId}`, updateData)
+        .then(res => {
+        })
+        .catch(error => console.log(error));
+    };
+
+    const handleCancel = () => {
+        navigate('/mypage/inquiry'); // 리스트 화면으로 이동
+    };
+
     return(
         <>
     <section className="contact-section">
     <h1 className="page-title">문의하기</h1>
     <p className="page-title2">궁금한 점이나 불편한 사항이 있으시면 아래 폼을 통해 문의해 주세요. 최대한 빠르게 답변 드리겠습니다.</p>
 
-    <form action="/submit-contact-form" method="post" className="contact-form">
+    <form onSubmit={handleSubmit}> 
+    <div className="contact-form">
+        <label htmlFor="subject">문의제목</label>
+        <input 
+            type="text" 
+            id="subject" 
+            name="subject" 
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+        />
 
-        <label htmlFor="subject">제목</label>
-        <input type="text" id="subject" name="subject" required />
-
-        <label htmlFor="message">메시지</label>
-        <textarea id="message" name="message" required></textarea>
+        <label htmlFor="message">문의내용</label>
+        <textarea 
+            id="message" 
+            name="message" 
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            required
+        ></textarea>
 
         <button type="submit">수정</button>
-        <button type="submit">취소</button>
+        <button type="button" onClick={handleCancel}>취소</button>
+        </div>
     </form>
     </section>
         
