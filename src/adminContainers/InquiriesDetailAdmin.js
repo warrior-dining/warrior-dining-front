4184964782f76c8f300c1e32c8a72124a63bac44
mirinae <enkim0401@naver.com>
2024-10-21 14:37:58 +0900

    [#72]관리자 문의사항 답변 유저 값 받아오기, css추가

--- src/adminContainers/InquiriesDetailAdmin.js
+++ src/adminContainers/InquiriesDetailAdmin.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams} from "react-router-dom";
 import axios from "axios";
+import { useAuth } from "../context/AuthContext";
 import '../css/InquiriesDetailAdmin.css';
 
 const host = "http://localhost:8080/api/admin/inquiries/";
@@ -11,11 +12,11 @@ const InquirieDtailsAdmin = () => {
     const inquiriesId = Number(id);
     const [data, setData] = useState(null);
     const [content, setContent] = useState("");
+    const {sub} = useAuth();
 
     useEffect(() => {
         axios.get(host + inquiriesId)
             .then(res => {
-                console.log(res);
                 setData(res.data.results);
                 setContent(res.data.results.answer ? res.data.results.answer.content : ""); // 초기 content 설정
             })
@@ -28,13 +29,20 @@ const InquirieDtailsAdmin = () => {
 
     const SubmitEvent = e => {
         e.preventDefault();
-        axios.post(host + inquiriesId, { "content": content })
+        axios.post(host + inquiriesId, { content: content, email: sub })
             .then(res => {
                 // 필요 시 추가 로직
                 window.location.reload();
             })
             .catch(error => console.log(error));
     }
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault(); // 새 줄 추가 방지
+            SubmitEvent(e); // 폼 제출
+        }
+    };
 
     const isAnswered = data.answer && data.answer.content;
 
@@ -56,8 +64,14 @@ const InquirieDtailsAdmin = () => {
                         <div className="reply-containers">
                             <h4>답변 작성</h4>
                             <form onSubmit={SubmitEvent}>
-                                <textarea placeholder="여기에 답변을 작성하세요..." value={content} name="text" onChange={(e) => setContent(e.target.value)} readOnly={isAnswered}/>
-                                <div  style={{ display: 'flex', justifyContent: 'flex-end', gap: '10px' }}>
+                                <textarea 
+                                    placeholder="여기에 답변을 작성하세요..." 
+                                    value={content} name="text" 
+                                    onChange={(e) => setContent(e.target.value)} 
+                                    onKeyDown={handleKeyDown} 
+                                    readOnly={isAnswered}
+                                />
+                                <div className="backList">
                                     {!isAnswered ? <button type="submit">답변 저장</button> : null}
                                     <button type="button" onClick={() => {
                                         window.location.href = '/admin/inquiries';
